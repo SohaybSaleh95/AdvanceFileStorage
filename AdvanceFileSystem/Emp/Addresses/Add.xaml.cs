2762a73cfe44fbb9f3c1ffbf37f6da7bfb842672
Aya Mohamad <aya96jbir@gmail.com>
2017-07-10 13:43:21 +0300

    ksdlksjf

--- AdvanceFileSystem/Emp/Addresses/Add.xaml.cs
+++ AdvanceFileSystem/Emp/Addresses/Add.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using AdvanceFileSystem.classes;
 
 namespace AdvanceFileSystem.Emp.Addresses
 {
@@ -23,6 +25,40 @@ namespace AdvanceFileSystem.Emp.Addresses
         public Add()
         {
             InitializeComponent();
+
+            address();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            string address = addressBox.Text;
+
+            MySqlConnection conn = Connection.Connect();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT INTO address (address) VALUES('" + address + "');";
+
+            cmd.ExecuteNonQuery();
+            this.address();
+            addressBox.Clear();
+        }
+
+        private void address ()
+        {
+            addresstable.Items.Clear();
+            MySqlConnection conn = Connection.Connect();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * FROM address";
+            MySqlDataReader Reader = cmd.ExecuteReader();
+            while (Reader.Read())
+            {
+                Address add = new Address();
+                add.Id = Reader.GetInt32("id");
+                add.Name = Reader.GetString("address");
+
+                addresstable.Items.Add(add);
+
+            }
+            Reader.Close();
         }
     }
 }
